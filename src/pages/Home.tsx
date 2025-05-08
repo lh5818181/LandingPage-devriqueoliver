65a06e456dc387b88622eafb5df26c5aba65ed13
Luis <lh5818181@gmail.com>
2025-05-08 19:59:37 -0300

    modificando

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,6 @@
-// src/pages/Home.tsx
-import React from 'react';
+
 import { HeroSection } from '../components/hero/Hero';
 import { DiagonalDivider } from '../components/SectionDivider';
-import { ValuePropsSection } from '../components/value/ValuePropsSection';
 import { ProcessSection } from '../components/process/ProcessSection';
 import { ContactSection } from '../components/contact/ContactSection';
 import { ServicesSection } from '../components/services/ServicesSection';
@@ -13,9 +11,8 @@ export function Home() {
     <>
       <Navbar />
       <HeroSection />
-      <ServicesSection />
       <DiagonalDivider />
-      <ValuePropsSection />
+      <ServicesSection />
       <DiagonalDivider style={{ transform: 'rotate(180deg)' }} />
       <ProcessSection />
       <ContactSection />
